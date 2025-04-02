a744cae607d2b4515703a6ac29bd8ca12ff4b9d8
Irina Emelianova <irina.m.emelianova@gmail.com>
2025-04-02 13:28:09 +0300

    Update file

--- web_scrapping.py
+++ web_scrapping.py
@@ -7,13 +7,20 @@ response = requests.get('https://habr.com/ru/articles/')
 soup = bs4.BeautifulSoup(response.text, features='lxml')
 
 all_articles = soup.find_all(class_='tm-article-snippet')
-articles_list = []
+parsed_data = []
 for article in all_articles:
+    link = ('https://habr.com' + article.find('h2').find('a').get('href'))
+    response = requests.get(link)
+    soup = bs4.BeautifulSoup(response.text, features='lxml')
     for word in KEYWORDS:
-        if article.text.lower().find(word) > 0 <= 1:
-            data = article.find('time').get('title')
-            header = article.find('h2').find('span').text
-            link = article.find('h2').find('a').get('href')
-            if link not in articles_list:
-                articles_list.append(f'{data}-{header}-{'https://habr.com' + link}')
-print(articles_list)
+        if article.text.lower().find(word) > 0:
+            article_time = article.find('time').get('title')
+            article_header = article.find('h2').find('span').text
+            article_link = ('https://habr.com' + article.find('h2').find('a').get('href'))
+            parsed_data.append({
+                'time': article_time,
+                'header': article_header,
+                'link': article_link,
+            })
+
+print(parsed_data)
